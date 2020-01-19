7304f7c92efb1a89aaed904ef020d489d2b88e62
Bruno Morais <brunosmmm@gmail.com>
2020-01-19 17:45:00 -0500

    some logging

--- dictator/config.py
+++ dictator/config.py
@@ -1,5 +1,13 @@
 """Validate test configuration."""
 
+VERBOSITY = {"error": 3, "warning": 2, "info": 1, "debug": 0}
+VERBOSITY_HEADERS = {
+    "error": "ERROR",
+    "warning": "WARNING",
+    "info": "",
+    "debug": "DEBUG",
+}
+
 
 class ConfigurationError(Exception):
     """Test configuration error."""
@@ -19,10 +27,29 @@ class DeferValidation(Exception):
         return self._depends
 
 
-def validate_config(config, required_keys, optional_keys=None):
+def _default_logger(msg, severity, verbosity):
+    """Default logger."""
+    if VERBOSITY[severity] >= VERBOSITY[verbosity]:
+        print(
+            "{}{}".format(
+                "{}{}".format(
+                    VERBOSITY_HEADERS[severity],
+                    ": " if VERBOSITY_HEADERS[severity] else "",
+                ),
+                msg,
+            )
+        )
+
+
+def validate_config(
+    config, required_keys, optional_keys=None, verbosity="error", log_fn=None
+):
     """Validate configuration."""
     if not isinstance(config, dict):
         raise TypeError("configuration must be a dictionary")
+
+    # pass validation config args down
+    vargs = {"verbosity": verbosity}
 
     for key in required_keys:
         if key not in config:
@@ -38,7 +65,11 @@ def validate_config(config, required_keys, optional_keys=None):
             or optional_keys is None
         ):
             # warning, unknown key
-            print(f"DEBUG: unknown key '{key}'")
+            if log_fn is not None:
+                _log = log_fn
+            else:
+                _log = _default_logger
+            _log(f"unknown key: '{key}'", "warning", verbosity)
             continue
 
         if key in required_keys:
@@ -52,7 +83,9 @@ def validate_config(config, required_keys, optional_keys=None):
             transformed_config[key] = value
         else:
             try:
-                new_value = key_loc[key](value, **transformed_config)
+                new_value = key_loc[key](
+                    value, _validator_args=vargs, **transformed_config
+                )
                 if new_value is None:
                     transformed_config[key] = value
                 else:
@@ -67,7 +100,9 @@ def validate_config(config, required_keys, optional_keys=None):
         else:
             key_loc = optional_keys
         try:
-            transform = key_loc[key](config[key], **transformed_config)
+            transform = key_loc[key](
+                config[key], _validator_args=vargs, **transformed_config
+            )
         except DeferValidation as ex:
             # deferred validation still not done, failure
             readable_depends = ", ".join(ex.depends)
