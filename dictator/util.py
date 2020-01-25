1f4f360c969e1ebd612f667fa94112024ea2cf9d
Bruno Morais <brunosmmm@gmail.com>
2020-01-24 21:30:00 -0500

    missing actual call to functions

--- dictator/util.py
+++ dictator/util.py
@@ -166,27 +166,27 @@ class KeyDependency(Validator):
 
 def default_validate_int(_value, **kwargs):
     """Default integer validator."""
-    return DEFAULT_VALIDATORS["integer"]
+    return DEFAULT_VALIDATORS["integer"](_value, **kwargs)
 
 
 def default_validate_pos_int(_value, **kwargs):
     """Default positive integer validator."""
-    return DEFAULT_VALIDATORS["positive_integer"]
+    return DEFAULT_VALIDATORS["positive_integer"](_value, **kwargs)
 
 
 def default_validate_str(_value, **kwargs):
     """Default string validator."""
-    return DEFAULT_VALIDATORS["string"]
+    return DEFAULT_VALIDATORS["string"](_value, **kwargs)
 
 
 def default_validate_list(_value, **kwargs):
     """Default list validator."""
-    return DEFAULT_VALIDATORS["list"]
+    return DEFAULT_VALIDATORS["list"](_value, **kwargs)
 
 
 def default_validate_dict(_value, **kwargs):
     """Default dict validator."""
-    return DEFAULT_VALIDATORS["dict"]
+    return DEFAULT_VALIDATORS["dict"](_value, **kwargs)
 
 
 def build_validate_choice(*choices):
