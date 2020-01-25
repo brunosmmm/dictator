ad82d35b85fb61e6877e0a5579d1fbfaa8b2a185
Bruno Morais <brunosmmm@gmail.com>
2020-01-25 09:42:12 -0500

    rework default validator API

--- dictator/default.py
+++ dictator/default.py
@@ -11,89 +11,87 @@ from dictator.validators import (
     validate_int_percent,
 )
 
-VALIDATE_DECORATORS_NOARGS = (
-    validate_integer,
-    validate_positive_integer,
-    validate_string,
-    validate_list,
-    validate_dict,
-    validate_int_percent,
-)
 
-VALIDATE_DECORATORS_ARGS = (ValidateChoice, ValidateIntRange)
+class _DefaultValidators:
+    """Default validators."""
 
+    VALIDATE_DECORATORS_NOARGS = (
+        validate_integer,
+        validate_positive_integer,
+        validate_string,
+        validate_list,
+        validate_dict,
+        validate_int_percent,
+    )
 
-def _make_default(decorator):
-    @decorator
-    def _validate_fn(_value, **kwargs):
-        return _value
+    DEFAULT_NAMES = [
+        "_".join(dec.__name__.split("_")[1:])
+        for dec in VALIDATE_DECORATORS_NOARGS
+    ]
 
-    return _validate_fn
+    VALIDATE_DECORATORS_ARGS = (ValidateChoice, ValidateIntRange)
 
+    DEFAULT_VALIDATOR_BY_TYPE = {
+        int: "integer",
+        str: "string",
+        list: "list",
+        dict: "dict",
+    }
 
-def _make_default_with_args(decorator):
-    def _outer_validate_fn(*args):
-        @decorator(*args)
+    def __init__(self):
+        """Initialize."""
+
+        self._DEFAULT_VALIDATORS = {
+            name: self._make_default(dec)
+            for name, dec in zip(
+                self.DEFAULT_NAMES, self.VALIDATE_DECORATORS_NOARGS
+            )
+        }
+
+        self._DEFAULT_VALIDATOR_BUILDERS = {
+            dec.get_default_name(): self._make_default_with_args(dec)
+            for dec in self.VALIDATE_DECORATORS_ARGS
+        }
+
+    @staticmethod
+    def _make_default(decorator):
+        """Build default validator function."""
+
+        @decorator
         def _validate_fn(_value, **kwargs):
             return _value
 
         return _validate_fn
 
-    return _outer_validate_fn
+    @staticmethod
+    def _make_default_with_args(decorator):
+        """Build default validator function builder."""
+
+        def _outer_validate_fn(*args):
+            @decorator(*args)
+            def _validate_fn(_value, **kwargs):
+                return _value
+
+            return _validate_fn
+
+        return _outer_validate_fn
+
+    def get_by_type(self, _type):
+        """Get default validator by type."""
+        if _type in self.DEFAULT_VALIDATOR_BY_TYPE:
+            return getattr(self, self.DEFAULT_VALIDATOR_BY_TYPE[_type])
+
+        raise TypeError("default validator not available for type f'{_type}'")
+
+    def __getattr__(self, name):
+        """Get attribute."""
+        if name in self._DEFAULT_VALIDATORS:
+            return self._DEFAULT_VALIDATORS[name]
+
+        if name in self._DEFAULT_VALIDATOR_BUILDERS:
+            return self._DEFAULT_VALIDATOR_BUILDERS[name]
+
+        raise AttributeError(f"default validator not available for '{name}'")
 
 
-_DEFAULT_NAMES = [
-    "_".join(dec.__name__.split("_")[1:]) for dec in VALIDATE_DECORATORS_NOARGS
-]
-
-DEFAULT_VALIDATORS = {
-    name: _make_default(dec)
-    for name, dec in zip(_DEFAULT_NAMES, VALIDATE_DECORATORS_NOARGS)
-}
-
-DEFAULT_VALIDATOR_BUILDERS = {
-    dec.get_default_name(): _make_default_with_args(dec)
-    for dec in VALIDATE_DECORATORS_ARGS
-}
-
-DEFAULT_VALIDATOR_BY_TYPE = {
-    int: "integer",
-    str: "string",
-    list: "list",
-    dict: "dict",
-}
-
-
-def default_validate_int(_value, **kwargs):
-    """Default integer validator."""
-    return DEFAULT_VALIDATORS["integer"](_value, **kwargs)
-
-
-def default_validate_pos_int(_value, **kwargs):
-    """Default positive integer validator."""
-    return DEFAULT_VALIDATORS["positive_integer"](_value, **kwargs)
-
-
-def default_validate_str(_value, **kwargs):
-    """Default string validator."""
-    return DEFAULT_VALIDATORS["string"](_value, **kwargs)
-
-
-def default_validate_list(_value, **kwargs):
-    """Default list validator."""
-    return DEFAULT_VALIDATORS["list"](_value, **kwargs)
-
-
-def default_validate_dict(_value, **kwargs):
-    """Default dict validator."""
-    return DEFAULT_VALIDATORS["dict"](_value, **kwargs)
-
-
-def build_validate_choice(*choices):
-    """Build a choice validator on the fly."""
-    return DEFAULT_VALIDATOR_BUILDERS["choice"](*choices)
-
-
-def build_validate_int_range(start, end):
-    """Build integer range validator on the fly."""
-    return DEFAULT_VALIDATOR_BUILDERS["int_range"](start, end)
+DEFAULT_VALIDATORS = _DefaultValidators()
