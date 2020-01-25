a74fc580f2f82146a70d1d2e644cf66401bc7abe
Bruno Morais <brunosmmm@gmail.com>
2020-01-24 21:04:31 -0500

    add more tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,13 +2,7 @@
 
 import pytest
 
-from dictator.util import (
-    default_validate_int,
-    default_validate_str,
-    default_validate_pos_int,
-    default_validate_list,
-    default_validate_dict,
-)
+from dictator.util import DEFAULT_VALIDATORS, DEFAULT_VALIDATOR_BUILDERS
 from dictator.config import validate_config, ConfigurationError
 
 
@@ -26,8 +20,8 @@ def test_validate_int():
     TEST_CONFIG = {"myValue": 42, "myHexValue": "0x100"}
     TEST_CONFIG_ERR = {"myValue": "str"}
     TEST_CONFIG_REQ = {
-        "myValue": default_validate_int,
-        "myHexValue": default_validate_int,
+        "myValue": DEFAULT_VALIDATORS["integer"],
+        "myHexValue": DEFAULT_VALIDATORS["integer"],
     }
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
@@ -41,7 +35,7 @@ def test_validate_pos_int():
     """Validate positive integers."""
     TEST_CONFIG = {"myValue": 42}
     TEST_CONFIG_ERR = {"myValue": -42}
-    TEST_CONFIG_REQ = {"myValue": default_validate_pos_int}
+    TEST_CONFIG_REQ = {"myValue": DEFAULT_VALIDATORS["positive_integer"]}
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
 
@@ -55,7 +49,7 @@ def test_validate_str():
     TEST_CONFIG = {"myValue": "str"}
     TEST_CONFIG_ERR = {"myValue": 42}
     TEST_CONFIG_REQ = {
-        "myValue": default_validate_str,
+        "myValue": DEFAULT_VALIDATORS["string"],
     }
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
@@ -69,7 +63,7 @@ def test_validate_list():
     """Validate a list."""
     TEST_CONFIG = {"myList": [1, 2, 3]}
     TEST_CONFIG_ERR = {"myList": "other"}
-    TEST_CONFIG_REQ = {"myList": default_validate_list}
+    TEST_CONFIG_REQ = {"myList": DEFAULT_VALIDATORS["list"]}
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
 
@@ -81,7 +75,47 @@ def test_validate_dict():
     """Validate a list."""
     TEST_CONFIG = {"myList": {0: "some"}}
     TEST_CONFIG_ERR = {"myList": "other"}
-    TEST_CONFIG_REQ = {"myList": default_validate_dict}
+    TEST_CONFIG_REQ = {"myList": DEFAULT_VALIDATORS["dict"]}
+
+    validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
+
+    with pytest.raises(ConfigurationError):
+        validate_config(TEST_CONFIG_ERR, TEST_CONFIG_REQ)
+
+
+def test_validate_choice():
+    """Validate a list."""
+    TEST_CONFIG = {"myList": "bla"}
+    TEST_CONFIG_ERR = {"myList": "other"}
+    TEST_CONFIG_REQ = {
+        "myList": DEFAULT_VALIDATOR_BUILDERS["choice"]("bla", "boo")
+    }
+
+    validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
+
+    with pytest.raises(ConfigurationError):
+        validate_config(TEST_CONFIG_ERR, TEST_CONFIG_REQ)
+
+
+def test_validate_intrange():
+    """Validate a list."""
+    TEST_CONFIG = {"myList": 42}
+    TEST_CONFIG_ERR = {"myList": 0}
+    TEST_CONFIG_REQ = {
+        "myList": DEFAULT_VALIDATOR_BUILDERS["int_range"](40, 46)
+    }
+
+    validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
+
+    with pytest.raises(ConfigurationError):
+        validate_config(TEST_CONFIG_ERR, TEST_CONFIG_REQ)
+
+
+def test_validate_intpercent():
+    """Validate a list."""
+    TEST_CONFIG = {"myList": 42}
+    TEST_CONFIG_ERR = {"myList": 200}
+    TEST_CONFIG_REQ = {"myList": DEFAULT_VALIDATORS["int_percent"]}
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
 
