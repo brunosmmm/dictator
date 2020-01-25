311975c38d1e3534c882573dd80d31bd2b657921
Bruno Morais <brunosmmm@gmail.com>
2020-01-25 10:33:54 -0500

    use defaults

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -2,26 +2,15 @@
 
 import pytest
 from dictator.config import validate_config, ConfigurationError
+from dictator.default import DEFAULT_VALIDATORS
 from dictator.util import KeyDependency, KeyDependencyMap
-
-
-@KeyDependency("myDependency")
-def _validate_my_key(_value, **kwargs):
-    """Validate myKey."""
-    return _value
-
-
-@KeyDependencyMap(someValue="someKey", otherValue="otherKey")
-def _validate_my_key_mapped(_value, **kwargs):
-    """Validate myKey depending on value."""
-    return _value
 
 
 def test_dependency():
     """Test key dependency."""
     TEST_CONFIG = {"myKey": "someValue", "myDependency": 42}
     TEST_CONFIG_ERR = {"myKey": "someValue"}  # missing dependency
-    TEST_CONFIG_REQ = {"myKey": _validate_my_key}
+    TEST_CONFIG_REQ = {"myKey": DEFAULT_VALIDATORS.dependency("myDependency")}
     TEST_CONFIG_OPT = {"myDependency": int}
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ, TEST_CONFIG_OPT)
@@ -34,7 +23,11 @@ def test_dependency_map():
     """Test key dependency."""
     TEST_CONFIG = {"myKey": "someValue", "someKey": 42, "otherKey": "bla"}
     TEST_CONFIG_ERR = {"myKey": "someValue"}  # missing dependency
-    TEST_CONFIG_REQ = {"myKey": _validate_my_key_mapped}
+    TEST_CONFIG_REQ = {
+        "myKey": DEFAULT_VALIDATORS.dependency_map(
+            someValue="someKey", otherValue="otherKey"
+        )
+    }
     TEST_CONFIG_OPT = {"someKey": int, "otherKey": str}
 
     validate_config(TEST_CONFIG, TEST_CONFIG_REQ, TEST_CONFIG_OPT)
