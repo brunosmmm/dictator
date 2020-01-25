15422895f75be0f8974d4dbb8da5f6441b758a5c
Bruno Morais <brunosmmm@gmail.com>
2020-01-25 10:49:39 -0500

    test sub-configurations

---
+++ tests/test_sub.py
@@ -0,0 +1,31 @@
+"""Test sub-confiurations."""
+
+import pytest
+from dictator.default import DEFAULT_VALIDATORS
+from dictator.config import validate_config, ConfigurationError
+
+
+def test_sub_list():
+    """Test sub-configuration in list."""
+    TEST_CONFIG = {"myStuff": [{"value": 42}, {"value": 46}]}
+    TEST_CONFIG_ERR = {"myStuff": [{"value": 42}, {"value": -50}]}
+    SUB_REQ = {"value": DEFAULT_VALIDATORS.positive_integer}
+    TEST_CONFIG_REQ = {"myStuff": DEFAULT_VALIDATORS.sub_list(SUB_REQ)}
+
+    validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
+
+    with pytest.raises(ConfigurationError):
+        validate_config(TEST_CONFIG_ERR, TEST_CONFIG_REQ)
+
+
+def test_sub_dict():
+    """Test sub-configuration in dict."""
+    TEST_CONFIG = {"myStuff": {"value": 42}}
+    TEST_CONFIG_ERR = {"myStuff": {"value": -50}}
+    SUB_REQ = {"value": DEFAULT_VALIDATORS.positive_integer}
+    TEST_CONFIG_REQ = {"myStuff": DEFAULT_VALIDATORS.sub_dict(SUB_REQ)}
+
+    validate_config(TEST_CONFIG, TEST_CONFIG_REQ)
+
+    with pytest.raises(ConfigurationError):
+        validate_config(TEST_CONFIG_ERR, TEST_CONFIG_REQ)
