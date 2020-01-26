06f2b6c51ff38ccaef2129c23eacb1f038ce8900
Bruno Morais <brunosmmm@gmail.com>
2020-01-26 10:31:47 -0500

    increase coverage

--- tests/test_malformed.py
+++ tests/test_malformed.py
@@ -19,6 +19,28 @@ def test_invalid_req():
 
     TEST_CONFIG = {0: "test"}
     TEST_REQ = {0: str}
+    TEST_OPT = {1: str}
 
     with pytest.raises(KeyDeclarationError):
         validate_config(TEST_CONFIG, TEST_REQ)
+
+    with pytest.raises(KeyDeclarationError):
+        validate_config(TEST_CONFIG, {}, TEST_OPT)
+
+
+def test_invalid_config():
+    """Test invalid config description."""
+    TEST_CONFIG = [1, 2, 3]
+    TEST_CONFIG_1 = {"some": "key"}
+    TEST_REQ = {}
+    TEST_REQ_1 = []
+    TEST_OPT_1 = []
+
+    with pytest.raises(TypeError):
+        validate_config(TEST_CONFIG, TEST_REQ)
+
+    with pytest.raises(TypeError):
+        validate_config(TEST_CONFIG_1, TEST_REQ_1)
+
+    with pytest.raises(TypeError):
+        validate_config(TEST_CONFIG_1, TEST_REQ, TEST_OPT_1)
