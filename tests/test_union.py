6ced44662a63857da67d4d4eacb7b64a52567b2e
Bruno Morais <brunosmmm@gmail.com>
2020-01-31 17:28:27 -0500

    test

---
+++ tests/test_union.py
@@ -0,0 +1,20 @@
+"""Test validator union."""
+
+import pytest
+from dictator.validators.util import ValidateUnion
+from dictator.errors import ValidationError
+from dictator.config import validate_config
+
+
+def test_union():
+    """Test union."""
+
+    TEST_CONFIG_1 = {"myKey": 1}
+    TEST_CONFIG_2 = {"myKey": False}
+    TEST_CONFIG_REQ = {"myKey": ValidateUnion(int, bool)}
+
+    validate_config(TEST_CONFIG_1, TEST_CONFIG_REQ)
+    validate_config(TEST_CONFIG_2, TEST_CONFIG_REQ)
+
+    with pytest.raises(ValidationError):
+        validate_config({"myKey": []}, TEST_CONFIG_REQ)
