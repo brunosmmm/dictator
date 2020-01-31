9854694ef8c9fdab92b2ff1fcf686b2102738bf9
Bruno Morais <brunosmmm@gmail.com>
2020-01-31 17:48:55 -0500

    test more

--- tests/test_combinations.py
+++ tests/test_combinations.py
@@ -1,9 +1,10 @@
 """Test validator combinations."""
 
 import pytest
-from dictator.validators.util import ValidateUnion
+from dictator.validators.util import ValidateUnion, InvertValidation
 from dictator.errors import ValidationError
 from dictator.config import validate_config
+from dictator.validators.base import validate_string
 
 
 def test_union():
@@ -18,3 +19,23 @@ def test_union():
 
     with pytest.raises(ValidationError):
         validate_config({"myKey": []}, TEST_CONFIG_REQ)
+
+
+def test_negate():
+    """Test negation."""
+
+    TEST_CONFIG_1 = {"myKey": False}
+    TEST_CONFIG_REQ_1 = {"myKey": InvertValidation(int)}
+    TEST_CONFIG_REQ_2 = {"myKey": InvertValidation(bool)}
+
+    validate_config(TEST_CONFIG_1, TEST_CONFIG_REQ_1)
+
+    with pytest.raises(ValidationError):
+        validate_config(TEST_CONFIG_1, TEST_CONFIG_REQ_2)
+
+    @InvertValidation()
+    @validate_string
+    def _validator(_value, **kwargs):
+        return _value
+
+    validate_config(TEST_CONFIG_1, {"myKey": _validator})
