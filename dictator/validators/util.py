bdbeaf32222cb8c9cd6e74170211835e1024cbf0
Bruno Morais <brunosmmm@gmail.com>
2020-01-31 17:48:49 -0500

    implement as validator class

--- dictator/validators/util.py
+++ dictator/validators/util.py
@@ -7,18 +7,53 @@ from dictator.validators import Validator
 from dictator.validators.base import DEFAULT_VALIDATOR_BY_TYPE
 
 
-def invert_condition(fn: Callable) -> Callable:
+class InvertValidation(Validator):
     """Invert validation condition."""
 
-    def _wrapper(_value, **kwargs):
-        try:
-            fn(_value, **kwargs)
-        except ValidationError:
-            return _value
+    def __init__(self, condition=None):
+        """Initialize."""
+        if (
+            condition is not None
+            and not isinstance(condition, type)
+            and not callable(condition)
+        ):
+            raise TypeError(
+                "condition must either be: None, a type or a callable"
+            )
+        self._condition = condition
 
-        raise ValidationError("validation passed where it shouldn't have")
+    def validate(self, _value, **kwargs):
+        """Perform validation."""
+        if self._condition is not None:
+            try:
+                _ = (
+                    DEFAULT_VALIDATOR_BY_TYPE[self._condition].validate(
+                        _value, **kwargs
+                    )
+                    if isinstance(self._condition, type)
+                    else self._condition(_value, **kwargs)
+                )
+            except ValidationError:
+                return _value
 
-    return _wrapper
+            raise ValidationError("validation passed where it shouldn't have")
+
+    def __call__(self, fn):
+        """Use as decorator."""
+
+        def _wrapper(_value, **kwargs):
+            try:
+                fn(_value, **kwargs)
+            except ValidationError:
+                return _value
+
+            raise ValidationError("validation passed where it shouldn't have")
+
+        return _wrapper
+
+
+# alias
+invert_validation = InvertValidation()
 
 
 class ValidateUnion(Validator):
@@ -37,6 +72,7 @@ class ValidateUnion(Validator):
         kwargs
           Any other metadata
         """
+        super().__init__()
         for condition in conditions:
             if not callable(condition) and not isinstance(condition, type):
                 raise TypeError("condition must be a callable or a type")
