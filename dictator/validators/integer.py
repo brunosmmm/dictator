92d2628c78f0fc65792c24364a66dacc548011ef
Bruno Morais <brunosmmm@gmail.com>
2020-10-20 09:25:21 -0400

    Pre-validate

--- dictator/validators/integer.py
+++ dictator/validators/integer.py
@@ -1,7 +1,7 @@
 """Composite integer validators."""
 
 from dictator.validators import Validator
-from dictator.validators.base import ValidatorFactory, validate_integer
+from dictator.validators.base import ValidatorFactory, validate_integer_pre
 from dictator.errors import ValidationError
 from typing import Any, Union
 
@@ -29,7 +29,7 @@ class ValidateIntRange(Validator):
         self._start = start
         self._end = end
 
-    @validate_integer
+    @validate_integer_pre
     def validate(self, _value, **kwargs):
         """Perform validation."""
         if (self._start is not None and _value < self._start) or (
