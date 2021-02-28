20e8c3c69491f24927ac04217cb07e650c5d8cd4
Bruno Morais <brunosmmm@gmail.com>
2021-02-28 17:10:45 -0500

    Remove some options from setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     package_dir={"": "."},
-    package_data={},
-    include_package_data=True,
-    install_requires=[],
     author="Bruno Morais",
     author_email="brunosmmm@gmail.com",
     description="DICTionary ConfiguraTion vAlidaTOR",
-    scripts=[],
 )
